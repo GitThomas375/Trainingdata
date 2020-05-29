b6a417404ff35677e7506494f5e815a1e683a300
Thomas Weidmann <T.A.Weidmann@web.de>
2020-05-29 13:36:27 +0200

    include von Graph.pb2 nochmal verändert

--- solver.cpp
+++ solver.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <time.h>
 #include "gurobi_c++.h"
-#include "trainingData/proto/graph.pb.h"
+#include "trainingdata/proto/graph.pb.h"
 #include <iostream>
 #include <fstream>
 
